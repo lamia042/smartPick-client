bd5f87afeef2637471b8d3520eddb2e728d3dd77
Lamia <lamiaaktar1216@gmail.com>
2025-08-29 01:30:19 +0600

    fixed navbar design

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
 
   return (
     <div className="bg-[#155ca2] sticky top-0 z-50">
-      <div className="w-11/12 lg:w-10/12 mx-auto flex justify-between items-center py-4">
+      <div className="px-4 md:px-10 mx-auto flex justify-between items-center py-4 gap-4">
         {/* Logo */}
         <div className="flex items-center">
           <img
