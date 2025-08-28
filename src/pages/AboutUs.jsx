ad2cee6566ebac78e2e022fc13473211390f05f8
Lamia <lamiaaktar1216@gmail.com>
2025-08-29 00:04:24 +0600

    bg img is added in about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,30 +1,47 @@
 import React from "react";
 import pic1 from "../assets/pic1.png";
 import pic2 from "../assets/pic2.jpg";
-import pic3 from "../assets/pic3.jpg"
+import pic3 from "../assets/pic3.jpg";
+import bg from "../assets/bg.jpg";
 
 const AboutUs = () => {
   return (
-    <div className="max-w-6xl mx-auto py-12 px-4">
+    <div className="max-w-6xl mx-auto py-12 px-4 ">
       {/* Header */}
-      <h1 className="text-4xl font-bold text-center mb-6 text-gray-800">
-        About Us
-      </h1>
-      <p className="text-center text-gray-600 mb-12">
-        SmartPick is a platform designed to help users make informed decisions by sharing queries and recommendations on products. Our goal is to empower users with collective knowledge and reliable insights.
-      </p>
+      <div className="py-10 md:px-25 mb-6" style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}>
+        <h1 className="text-4xl font-bold text-center mb-6 text-gray-900">
+          About Us
+        </h1>
+        <p className="text-center font-bold text-white mb-12">
+          SmartPick is a platform designed to help users make informed decisions
+          by sharing queries and recommendations on products. Our goal is to
+          empower users with collective knowledge and reliable insights.
+        </p>
+      </div>
 
       {/* Mission Section */}
       <section className="mb-12">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">Our Mission</h2>
-        <p className="text-gray-600">
-          Our mission is to create a trusted community where users can ask questions, provide recommendations, and help each other make smarter choices. We believe in transparency, reliability, and empowering users with knowledge.
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+          Our Mission
+        </h2>
+        <p className="text-gray-600 text-center md:px-20">
+          Our mission is to create a trusted community where users can ask
+          questions, provide recommendations, and help each other make smarter
+          choices. We believe in transparency, reliability, and empowering users
+          with knowledge.
         </p>
       </section>
 
       {/* Team Section */}
       <section>
-        <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">Meet Our Team</h2>
+        <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">
+          Meet Our Team
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="text-center">
             <img
